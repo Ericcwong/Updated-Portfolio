d59491ecf8b0b67b3a5f248c5951fa8a41ee91ed
Eric Wong <ericwong12@live.com>
2020-01-19 19:06:02 -0700

    Home page layout almost complete

---
+++ assets/javascript/script.js
@@ -0,0 +1,6 @@
+//Footer Icons hover displays text
+$("#github").attr("title", "GitHub");
+$("#linkedin").attr("title", "LinkedIn");
+$("#instagram").attr("title", "InstaGram");
+$("#twitter").attr("title", "Twitter");
+
